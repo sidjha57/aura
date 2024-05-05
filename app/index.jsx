defdde8e9c266499c81ddbabba529ed3f5f83d9a
Siddharth Jha <sidjha57@gmail.com>
2024-05-05 23:15:05 +0530

    chore: home page done

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,52 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { Link } from "expo-router";
+import { ScrollView, StyleSheet, Text, View, Image } from "react-native";
+import { Link, router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import CustomButton from "./components/CustomButton";
 
 export default function App() {
 	return (
-		<View className="flex-1 items-center justify-center bg-white">
-			<Text className="text-black text-3xl font-pblack">Aura!</Text>
-			<StatusBar style="auto" />
-			<Link href="/home" className="text-blue-500">
-				Home
-			</Link>
-		</View>
+		<SafeAreaView className="bg-primary h-full">
+			<ScrollView
+				contentContainerStyle={{
+					height: "100%",
+				}}
+			>
+				<View className="flex w-full min-h-[85vh] justify-center  items-center px-4">
+					<Image
+						source={images.logo}
+						className="w-[130px] h-[84px]"
+						resizeMode="contain"
+					/>
+					<Image
+						source={images.cards}
+						className="max-w-[380px] h-[298px]"
+						resizeMode="contain"
+					/>
+					<View className="relative mt-5">
+						<Text className="text-3xl text-white font-bold text-center">
+							Discover Endless{"\n"} Possibilities with{" "}
+							<Text className="text-secondary-200">Aura</Text>
+						</Text>
+						<Image
+							source={images.path}
+							className="w-[134px] h-[15px] absolute -bottom-2 -right-8"
+							resizeMode="contain"
+						/>
+					</View>
+					<Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+						Where creativity meets innovation: embark on a journey of limitless
+						exploration with Aura
+					</Text>
+					<CustomButton
+						title="Continue with Email"
+						handlePress={() => router.push('/sign-in')}
+						containerStyles="w-full mt-7"
+					/>
+				</View>
+			</ScrollView>
+			<StatusBar backgroundColor="#161622" style="light" />
+		</SafeAreaView>
 	);
 }
