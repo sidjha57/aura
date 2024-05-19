ebce31b4bc34da43c39160ec5d033098ef4bfa40
Siddharth Jha <sidjha57@gmail.com>
2024-05-19 13:54:34 +0530

    feat: added sentry

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,44 @@
 import { useFonts } from "expo-font";
-import { SplashScreen, Stack } from "expo-router";
+import { SplashScreen, Stack, useNavigationContainerRef } from "expo-router";
 import React, { useEffect } from "react";
 import GlobalProvider from "../context/GlobalProvider";
+import * as Sentry from "@sentry/react-native";
+import { isRunningInExpoGo } from "expo";
+
+// Construct a new instrumentation instance. This is needed to communicate between the integration and React
+const routingInstrumentation = new Sentry.ReactNavigationInstrumentation();
+
+Sentry.init({
+	dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
+	environment: process.env.NODE_ENV,
+	debug: process.env.NODE_ENV !== "development" ? false : true,
+	integrations: [
+		new Sentry.ReactNativeTracing({
+			// Pass instrumentation to be used as `routingInstrumentation`
+			routingInstrumentation,
+			enableNativeFramesTracking: !isRunningInExpoGo(),
+			// ...
+		}),
+	],
+	tracesSampleRate: 1.0,
+	_experiments: {
+		// profilesSampleRate is relative to tracesSampleRate.
+		// Here, we'll capture profiles for 100% of transactions.
+		profilesSampleRate: 1.0,
+	},
+});
 
 SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
+	const ref = useNavigationContainerRef();
+
+	React.useEffect(() => {
+		if (ref) {
+			routingInstrumentation.registerNavigationContainer(ref);
+		}
+	}, [ref]);
+
 	const [fontsLoaded, error] = useFonts({
 		"Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
 		"Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
@@ -46,4 +79,4 @@ const RootLayout = () => {
 	);
 };
 
-export default RootLayout;
+export default Sentry.wrap(RootLayout);
